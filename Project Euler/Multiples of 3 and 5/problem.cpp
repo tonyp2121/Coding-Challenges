e7a97f5ad31f87481a6870424314562550b0764e
Anthony Pallone <pallone@unlv.nevada.edu>
2017-04-17 21:55:25 -0700

    Finished a Euler Problem

--- Project Euler/Multiples of 3 and 5/problem.cpp
+++ Project Euler/Multiples of 3 and 5/problem.cpp
@@ -2,13 +2,18 @@
 
 using namespace std;
 
-int sumMultiples3and5(int num)
+int sumMultiples3and5(int num);
 
 int main(void){
-
+int i = sumMultiples3and5(1000);
+cout << i << endl;
 return 0;
 }
 
 int sumMultiples3and5(int num){
-  
+  int sum = 0;
+  for(int i = 0; i < num; i++){
+    if (i % 3 == 0 || i % 5 == 0){sum+=i;}
+  }
+  return sum;
 }
