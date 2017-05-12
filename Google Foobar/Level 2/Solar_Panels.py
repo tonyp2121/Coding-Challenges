a4f39e301cd67b94c6490414629317fe40e4c30c
Anthony Pallone <pallone@unlv.nevada.edu>
2017-05-12 13:32:34 -0700

    Finished Solar Panels Problem

--- Google Foobar/Level 2/Solar_Panels.py
+++ Google Foobar/Level 2/Solar_Panels.py
@@ -1,12 +1,18 @@
+import random
+
 def answer(xs):
     Set = False
     Max = 1
     neg = []
-    if len(xs) == 0:
-        return str(0)
+    if len(xs) == 1:
+        return str(xs[0])
+    while 0 in xs:
+        xs.remove(0)
     for i in range(len(xs)):
         if(xs[i] < 0):
             neg.append(xs[i])
+    for i in range(len(neg)):
+        xs.remove(neg[i])
     neg.sort()
     if len(neg) % 2 == 0 and len(neg) > 0:
         Max = 1
@@ -19,17 +25,26 @@ def answer(xs):
         Set = True
         for i in range(len(neg)):
             Max *= neg[i]
+    Max = 1
     for i in range(len(xs)):
         if xs[i] > 0 and Set == False:
             Max *= xs[i]
             Set = True
         elif xs[i] > 0 and Set == True:
             Max *= xs[i]
+    if len(xs) == 0:
+        return str(0)
     return(str(Max))
 
-xs = [1, 0, 5, -2, -3]
+
+xs = [-1]
 print(answer(xs))
-xs = [-1, -4, 2, -2, -3]
+xs = [0,0]
 print(answer(xs))
-xs = []
-print(answer(xs))
+for j in range(5):
+    for i in range(j+5):
+        xs.append(random.randint(-9,9))
+    print (xs)
+    print(answer(xs)+ "\n")
+    xs = []
+    
