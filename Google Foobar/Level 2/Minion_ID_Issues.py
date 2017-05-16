4720630f581052d8e7272466d2f0d83a66773502
Anthony Pallone <pallone@unlv.nevada.edu>
2017-05-16 16:26:15 -0700

    Continued development on Minion ID Issues

--- Google Foobar/Level 2/Minion_ID_Issues.py
+++ Google Foobar/Level 2/Minion_ID_Issues.py
@@ -1,14 +1,56 @@
+import string
+digs = string.digits + string.letters
+
+def magic(numList):
+    s = ''.join(map(str, numList))
+    return int(s)
+
+def int2base(x, base):
+    if x < 0:
+        sign = -1
+    elif x == 0:
+        return digs[0]
+    else:
+        sign = 1
+
+    x *= sign
+    digits = []
+
+    while x:
+        digits.append(digs[x % base])
+        x /= base
+
+    if sign < 0:
+        digits.append('-')
+
+    digits.reverse()
+
+    return ''.join(digits)
+
 def answer(n,b): #where n is minion id and b is base
     length = len(n)
     values = map(int, n)
     descending = list(values)
     ascending = list(values)
-    descending.sort()
-    ascending.sort(reverse=True)
-    var1 = int(n,b)
-    var2 = []
-    for i in range (len(descending)):
-        var2.append(descending[i] - ascending[i])
-    print(var1)
+    descending.sort(reverse=True)
+    descending = "".join(str(x) for x in descending)
+    ascending.sort()
+    ascending = "".join(str(x) for x in ascending)
+    var1 = int(n,b)     #in our code X = descending Y = ascending z = var2 = x -y
+    var2 = int(ascending,b)
+    var3 = int(descending,b)
+    var4 = var3-var2  #var 4 is our z now now we need to do it forever.
+    var5 = int2base(var4,b)
     
-answer("11",2)
+    print(var1)
+    print(var2)
+    print(descending)
+    print(ascending)
+    print(var3)
+    print(int2base(var4,b))
+    count = 0
+    
+    Found = False;
+    #I think its meant to be recursive
+    
+answer("120",3)
