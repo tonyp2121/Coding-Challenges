96fd88e17a31bf73892997f1af34bc765149afa2
Anthony Pallone <pallone@unlv.nevada.edu>
2017-05-17 10:50:09 -0700

    Completed Minion ID Issues

--- Google Foobar/Level 2/Minion_ID_Issues.py
+++ Google Foobar/Level 2/Minion_ID_Issues.py
@@ -1,10 +1,6 @@
 import string
 digs = string.digits + string.letters
 
-def magic(numList):
-    s = ''.join(map(str, numList))
-    return int(s)
-
 def int2base(x, base):
     if x < 0:
         sign = -1
@@ -12,45 +8,44 @@ def int2base(x, base):
         return digs[0]
     else:
         sign = 1
-
     x *= sign
     digits = []
-
     while x:
         digits.append(digs[x % base])
         x /= base
-
     if sign < 0:
         digits.append('-')
-
     digits.reverse()
 
     return ''.join(digits)
 
 def answer(n,b): #where n is minion id and b is base
+    listOfZ = [n]
+    var5 = n
     length = len(n)
-    values = map(int, n)
-    descending = list(values)
-    ascending = list(values)
-    descending.sort(reverse=True)
-    descending = "".join(str(x) for x in descending)
-    ascending.sort()
-    ascending = "".join(str(x) for x in ascending)
-    var1 = int(n,b)     #in our code X = descending Y = ascending z = var2 = x -y
-    var2 = int(ascending,b)
-    var3 = int(descending,b)
-    var4 = var3-var2  #var 4 is our z now now we need to do it forever.
-    var5 = int2base(var4,b)
-    
-    print(var1)
-    print(var2)
-    print(descending)
-    print(ascending)
-    print(var3)
-    print(int2base(var4,b))
-    count = 0
-    
-    Found = False;
-    #I think its meant to be recursive
-    
-answer("120",3)
+    Found = False
+    Count = 0
+    while not False:
+        values = map(int, var5)
+        descending = list(values)
+        ascending = list(values)
+        descending.sort(reverse=True)
+        descending = "".join(str(x) for x in descending)
+        ascending.sort()
+        ascending = "".join(str(x) for x in ascending)
+        var1 = int(var5,b)     #in our code X = descending Y = ascending z = var2 = x -y
+        var2 = int(ascending,b)
+        var3 = int(descending,b)
+        var4 = var3-var2  #var 4 is our z now now we need to do it forever.
+        var5 = int2base(var4,b)
+        Count += 1
+        if len(var5) == 1:
+            return 1
+        for i in range(len(listOfZ)):
+            if var5 == listOfZ[i]:
+                return Count - i
+        var5.zfill(length)
+        print(var5)
+        listOfZ.append(var5)
+        
+print(answer("210022",3))
